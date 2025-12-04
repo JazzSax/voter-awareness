9237e7466a266c3cf761a98a85d8bfdd8767f45d
John Bernard Sahagun <jhnbrnrdshgn@gmail.com>
2025-12-05 03:13:46 +0800

    update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import { DATA } from "./content";
 function App() {
-  return <AboutPage data={DATA} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage data={DATA} />} />
+        <Route path="/about" element={<AboutPage data={DATA} />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
