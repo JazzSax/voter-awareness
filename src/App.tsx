19ff209a8991e22fa452d08071056a07d9d9431e
John Bernard Sahagun <jhnbrnrdshgn@gmail.com>
2025-12-05 04:01:31 +0800

    feat: add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,57 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import { DATA } from "./content";
+import Logo from "./assets/logo/logo.png";
+import { cn } from "./lib/utils";
 function App() {
   const [page, setPage] = useState("home");
-
-  useEffect(() => {
-    const getPageFromPath = () => {
-      const path = window.location.pathname.replace("/voter-awareness", "");
-      setPage(path === "/about" ? "about" : "home");
-    };
-
-    getPageFromPath();
-
-    const onPopState = () => getPageFromPath();
-    window.addEventListener("popstate", onPopState);
-
-    return () => window.removeEventListener("popstate", onPopState);
-  }, []);
+  const navigate = (target: string) => {
+    const url =
+      target === "home" ? "/voter-awareness/" : "/voter-awareness/about";
+    window.history.pushState(null, "", url);
+    setPage(target);
+  };
 
   const renderPage = () => {
     return page === "about" ? (
-      <HomePage data={DATA} />
-    ) : (
       <AboutPage data={DATA} />
+    ) : (
+      <HomePage data={DATA} />
     );
   };
-  return <div>{renderPage()}</div>;
+  return (
+    <>
+      <nav
+        className={cn(
+          "p-4 bg-white dark:bg-black shadow-md flex justify-between items-center font-sans",
+          page === "home" && "dark"
+        )}
+      >
+        <img src={Logo} alt="Logo" className="h-12" />
+
+        <ul className="flex space-x-6 text-black dark:text-white text-lg font-medium">
+          <li>
+            <button
+              onClick={() => navigate("home")}
+              className="hover:text-blue-600 transition-colors duration-300"
+            >
+              Home
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => navigate("about")}
+              className="hover:text-blue-600 transition-colors duration-300"
+            >
+              About
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <div>{renderPage()}</div>;
+    </>
+  );
 }
 
 export default App;
