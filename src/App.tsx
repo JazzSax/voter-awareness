6b4213537f7c2cc992fa75edd9ca9ae33480e27d
John Bernard Sahagun <jhnbrnrdshgn@gmail.com>
2025-12-05 03:02:03 +0800

    revert app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import { DATA } from "./content";
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage data={DATA} />} />
-        <Route path="/about" element={<AboutPage data={DATA} />} />
-      </Routes>
-    </Router>
-  );
+  return <AboutPage data={DATA} />;
 }
 
 export default App;
